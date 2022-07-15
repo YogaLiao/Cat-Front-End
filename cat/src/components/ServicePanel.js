b0bc15feb5415a17870991ad4a13bf63bcbda863
YogaLiao <liaoyujia0702@gmail.com>
2022-07-14 19:57:48 -0500

    add update function

--- cat/src/components/ServicePanel.js
+++ cat/src/components/ServicePanel.js
@@ -1,84 +1,158 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "@progress/kendo-theme-default/dist/all.css"; 
 import "react-modern-calendar-datepicker/lib/DatePicker.css";
 import { Calendar, utils } from "react-modern-calendar-datepicker";
 import { DropDownList } from "@progress/kendo-react-dropdowns";
+import axios from 'axios'
 
-function ServicePanel() {
-  const serviceInfo = [{
-    username: 'admin', displayName: 'admin', headline: 'Your pets are mine', service: 'housesitting', rate: 20,
-    service: "housesitting", note:'No', disable: ['2022/07/13', '2022/07/14', '2022/07/15', '2022/07/19', '2022/07/18']
-  },
-  {
-    username: 'admin', displayName: 'admin', headline: 'Your pets are mine', service: 'boarding', rate: 30,
-    service: "onboarding", note:'No', disable: ['2022/07/18', '2022/07/19', '2022/07/20', '2022/07/22']
-  }]
-  const [type, setType] = useState(serviceInfo[0].service)
-  const types = []
-  serviceInfo.map(x => types.push(x.service))
-  const filteredData = useMemo(()=> serviceInfo.filter(x => x.service == type))
-  const disable = filteredData[0].disable // insert db data here
-  const disabledDays = []
-  disable.map(day => {
-    const d = new Date(day)
-    disabledDays.push({
-      year: d.getFullYear(),
-      month: d.getMonth() + 1,
-      day: d.getDate()
-    })
-  })
-  const defaultValue = utils().getToday()
+function ServicePanel({userSignedIn}) {
+
+  userSignedIn = localStorage.getItem('user')
+  console.log(userSignedIn)
+  const userEndpoint = 'users'
+  const [serviceInfo, setServiceInfo] = useState([])
+  const [types, setTypes] = useState([])
+
+  const [type, setType] = useState("")
+  const [disabledDays, setDisabledDays] = useState([])
+
+  // const serviceInfo =
+  //   [{
+  //   username: 'admin', displayName: 'admin', headline: 'Your pets are mine', service: 'housesitting', rate: 20,
+  //   service: "housesitting", note:'No', disable: ['2022/07/13', '2022/07/14', '2022/07/15', '2022/07/19', '2022/07/18']
+  // },
+  // {
+  //   username: 'admin', displayName: 'admin', headline: 'Your pets are mine', service: 'boarding', rate: 30,
+  //   service: "onboarding", note:'No', disable: ['2022/07/18', '2022/07/19', '2022/07/20', '2022/07/22']
+  //   }]
+    useEffect(() => {
+      axios.get(process.env.REACT_APP_API_URL + userEndpoint)
+        .then(data => {
+          console.log(data.data)
+          let info = data.data.filter(x => x.username == userSignedIn)
+          console.log(info)
+          console.log(data.data[0].username)
+          setServiceInfo(info[0].services)
+          // setType(serviceInfo.service)
+          // const filteredData = serviceInfo.filter(x => x.service == type)
+          // setDisable(filteredData.disable )
+          let copy = []
+          serviceInfo.map(x => copy.push(x.service))
+          setTypes(copy)
+          setType(serviceInfo[0].service)
+          const disable = serviceInfo[0].disable
+          let copy1 = []
+          disable.map(day => {
+            const d = new Date(day)
+            copy1.push({
+              year: d.getFullYear(),
+              month: d.getMonth() + 1,
+              day: d.getDate()
+            })
+          })
+          setDisabledDays(copy1)
+        })
+      
+          
+        
+    }, [])
   
-  console.log(defaultValue)
+
+    const handleChange = (e) => {
+    setType(e.target.value)
+    const filteredData = serviceInfo.filter(x => x.service == type)
+    const disable = filteredData[0].disable
+    let copy = []
+    disable.map(day => {
+      const d = new Date(day)
+      copy.push({
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate()
+      })
+    })
+    setDisabledDays(copy)
+    }
+ 
+  console.log(serviceInfo)
+  console.log(types)
+  
+    
+    // const filteredData = useMemo(() => serviceInfo.filter(x => x.service == type))
+    // const disable = filteredData[0].disable
+    // const disabledDays = []
+    // disable.map(day => {
+    //   const d = new Date(day)
+    //   disabledDays.push({
+    //     year: d.getFullYear(),
+    //     month: d.getMonth() + 1,
+    //     day: d.getDate()
+    //   })
+    // })
+    const defaultValue = utils().getToday()
+  
+    console.log(defaultValue)
 
 
-  const [selectedDay, setSelectedDay] = useState(defaultValue);
-  const [selected, setSelected] = useState(false)
+    const [selectedDay, setSelectedDay] = useState(defaultValue);
+    const [selected, setSelected] = useState(false)
 
-  console.log(selected)
-  console.log(selectedDay)
+    console.log(selected)
+    console.log(selectedDay)
 
-  const handleDisabledSelect = disabledDay => {
-    console.log('Tried selecting a disabled day', disabledDay);
-  };
+    const handleDisabledSelect = disabledDay => {
+      console.log('Tried selecting a disabled day', disabledDay);
+    }
+  
+
 
   return (
-    <div className="service">
-      <div className="service-list">
-        <h1>Services</h1>
-        {serviceInfo.map(x => (
-          <div className="list">
-            <div className="left">
-              <h3>{x.service}</h3>
+    <>
+      {!serviceInfo
+        ? "Loading"
+        : <div className="service">
+      
+          {serviceInfo.length > 0
+          ? < div className="service-list">
+          <h1>Services</h1>
+          
+        
+          {serviceInfo.map(x => (
+            <div className="list">
+              <div className="left">
+                <h3>{x.service}</h3>
+              </div>
+              <div className="right">
+                <h3>$ {x.rate}</h3>
+                <p>per visit</p>
+              </div>
             </div>
-            <div className="right">
-              <h3>$ {x.rate}</h3>
-              <p>per visit</p>
-            </div>
-        </div>
-        ))}
+          ))}
         
         
-      </div>
-      <h1>Availability</h1>
-      <form>
-        <DropDownList className="k-form k-mb-4" data={types} onChange={e => setType(e.value)} />
+            </div>
+        : <h1>No Service Yet</h1>}
+          <h1>Availability</h1>
+          <form>
+            <DropDownList className="k-form k-mb-4" data={types} onChange={handleChange} />
           </form>
-    <Calendar
-      value={selectedDay}
-      onChange={d => {
-        setSelectedDay(d)
-        setSelected(true)
-      }}
-      minimumDate={utils().getToday()}
-      colorPrimary="#9b5de5"
-      calendarTodayClassName="custom-today-day"
-      calendarClassName="responsive-calendar"
-      disabledDays={disabledDays} // here we pass them
-      onDisabledDayError={handleDisabledSelect} // handle error
-      shouldHighlightWeekends
-      />
-      </div>
+          <Calendar
+            value={selectedDay}
+            onChange={d => {
+              setSelectedDay(d)
+              setSelected(true)
+            }}
+            minimumDate={utils().getToday()}
+            colorPrimary="#9b5de5"
+            calendarTodayClassName="custom-today-day"
+            calendarClassName="responsive-calendar"
+            disabledDays={disabledDays} // here we pass them
+            onDisabledDayError={handleDisabledSelect} // handle error
+            shouldHighlightWeekends
+          />
+        </div>
+      }
+      </>
   
   )
 }
--- cat/src/pages/Dashboard.js
+++ cat/src/pages/Dashboard.js
@@ -3,26 +3,30 @@ import ServicePanel from '../components/ServicePanel'
 import Calendar2 from '../components/Calendar2'
 import Nav from '../components/Nav'
 import axios from 'axios'
+import {useNavigate} from "react-router-dom"
 
-function Dashboard({ userSignedIn, setAccessToken, setUserSignedIn, setShowModal, showModal, setIsSignUp, isSignUp }) {
+function Dashboard({ accessToken,userSignedIn, setAccessToken, setUserSignedIn, setShowModal, showModal, setIsSignUp, isSignUp }) {
   userSignedIn = localStorage.getItem('user')
+  let navigate = useNavigate()
   console.log(userSignedIn)
-  const userEndpoint = 'users'
+
+  const userEndpoint = 'users/'
   const [userInfo, setUserInfo] = useState([])
+  const [userId, setUserId] = useState()
   const [formData, setFormData] = useState({
     username: userSignedIn,
-    first_name: "",
-    last_name: "",
-    dob_month: "",
-    dob_day: "",
-    dob_year: "",
-    address: "",
-    city: "",
-    state: "",
-    zipcode: "",
-    cell: "",
-    url: "",
-    about: ""
+    first_name: userInfo.first_name,
+    last_name: userInfo.last_name,
+    dob_month: userInfo.dob_month,
+    dob_day: userInfo.dob_day,
+    dob_year: userInfo.dob_year,
+    address: userInfo.address,
+    city: userInfo.city,
+    state: userInfo.state,
+    zipcode: userInfo.zipcode,
+    cell: userInfo.cell,
+    url: userInfo.url,
+    about: userInfo.about
   })
 
   const handleChange = (e) => {
@@ -42,180 +46,258 @@ function Dashboard({ userSignedIn, setAccessToken, setUserSignedIn, setShowModal
         let info = data.data.filter(x => x.username == userSignedIn)
         console.log(info)
         setUserInfo(info[0])
+        setFormData(userInfo)
+        setUserId(userInfo.id)
       })
   }, [])
+console.log(userInfo)
+  const [networkErrMsg, setNetworkErrMsg] = useState(null)
+  const [clientErrMsg, setClientErrMsg] = useState(null)
+
+  const statusCodeToErr = (responseObj) => {
+      setNetworkErrMsg(`Network Error of code: ${responseObj.status}`)
+      // TODO - console log the err message
+  }
+
+  const clientFormValidation = (formData) => {
+      const blankFields = Object.entries(formData)
+                                .filter(kv => kv[1] === '')
+      if (blankFields.length > 0) {
+          setClientErrMsg(`${blankFields[0][0]} can not be blank`)
+          return false
+      }
+      setClientErrMsg(null)
+      return true
+  }
+
+  const handleUpdate = (e) => {
+    e.preventDefault()
+    setNetworkErrMsg(null)
+
+      if (!clientFormValidation(formData)) {
+          return
+      }
+    
+    const apiUrl = process.env.REACT_APP_API_URL
+    console.log(`fetching with token ${accessToken}`)
+
+    fetch( apiUrl + userEndpoint + userId,       
+      {
+          method: 'PUT',
+          headers: {
+              'Content-Type':'application/json',
+              'Authorization':` Bearer ${accessToken}`
+          },
+          body: JSON.stringify(formData)
+      }
+)
+  .then(res => {
+      if (res.ok) {
+          return res.json()
+      } else {
+          statusCodeToErr(res)
+          return Promise.resolve(null)
+      }
+  })
+  .then(data => {
+      if (!data) {
+          console.log(`problem with network request: ${networkErrMsg}`)
+      } else {
+          
+          console.log(data)
+
+          // call to refresh the list
+          // set RefreshCounter(refreshCounter + 1)
+      }
+  })
+
+    console.log(formData)
+    navigate('/dashboard')
+  }
 
   return (
-    <div className="dashboard">
-      <Nav
-        setShowModal={setShowModal}
-        showModal={showModal}
-        setIsSignUp={setIsSignUp}
-        isSignUp={isSignUp}
-        userSignedIn={userSignedIn}
-        setUserSignedIn={setUserSignedIn}
-        setAccessToken={setAccessToken}
-      />
+  <>
+    {!userInfo
+      ? "Loading"
+      : <div className="dashboard">
+        <Nav
+          setShowModal={setShowModal}
+          showModal={showModal}
+          setIsSignUp={setIsSignUp}
+          isSignUp={isSignUp}
+          userSignedIn={userSignedIn}
+          setUserSignedIn={setUserSignedIn}
+          setAccessToken={setAccessToken}
+        />
 
 
         <div className='main-content'>
           <section className="top-container">
             <div>
-              {!userInfo
-                ? "Loading"
-              : <div className="panel">
-                <div className = 'img-container'>
+              
+              <div className="panel">
+                <div className='img-container'>
                   <img src={userInfo.url}></img>
                 </div>
-                  <div className='info'>
-                  <p className = "name">{userInfo.first_name} {userInfo.last_name}</p>
+                <div className='info'>
+                  <p className="name">{userInfo.first_name} {userInfo.last_name}</p>
                   <p>{userInfo.city}, {userInfo.state}</p>
-                  </div>
                 </div>
-                }
-          </div>
-        </section>
-        <section className='bottom-container'>
+              </div>
+                
+            </div>
+          </section>
+          <section className='bottom-container'>
             <div className="servicePanel">
+            
               <ServicePanel
-                userInfo = {userInfo}
+                userSignedIn={userSignedIn}
               />
             </div>
                 
-          <div className="profile">
-            <h1>Edit Profile</h1>
-          <form>
-          <section>
-            <label htmlFor='first_name'>First Name</label>
-            <input
-              id="first_name"
-              type="text"
-              name="first_name"
-              placeholder={userInfo.first_name}
-              required={true}
-              value={formData.first_name}
-              onChange = {handleChange}
-            />
-            <label htmlFor='last_name'>Last Name</label>
-            <input
-              id="last_name"
-              type="text"
-              name="last_name"
-              placeholder={userInfo.last_name}
-              required={true}
-              value={formData.last_name}
-              onChange = {handleChange}
-            />
-            <label>Birthday</label>
-            <div className='multiple-input-container'>
-              <input
-                id="dob_month"
-                type="number"
-                name="dob_month"
-                placeholder={userInfo.dob_month}
-                required={true}
-                value={formData.dob_month}
-                onChange = {handleChange}
-              />
-            <input
-              id="dob_day"
-              type="number"
-              name="dob_day"
-              placeholder={userInfo.dob_day}
-              required={true}
-              value={formData.dob_day}
-              onChange = {handleChange}
-            />
-            <input
-              id="dob_year"
-              type="number"
-              name="dob_year"
-              placeholder={userInfo.dob_year}
-              required={true}
-              value={formData.dob_year}
-              onChange = {handleChange}
-              />
-            </div>
+            <div className="profile">
+              <h1>Edit Profile</h1>
+              <form onSubmit={handleUpdate}>
+                <section>
+                  <label htmlFor='first_name'>First Name</label>
+                  <input
+                    id="first_name"
+                    type="text"
+                    name="first_name"
+                    placeholder={userInfo.first_name}
+                      // required={true}
+                      // defaultValue={userInfo.first_name}
+                    value={formData.first_name}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor='last_name'>Last Name</label>
+                  <input
+                    id="last_name"
+                    type="text"
+                    name="last_name"
+                    placeholder={userInfo.last_name}
+                    // required={true}
+                    value={formData.last_name}
+                    onChange={handleChange}
+                  />
+                  <label>Birthday</label>
+                  <div className='multiple-input-container'>
+                    <input
+                      id="dob_month"
+                      type="number"
+                      name="dob_month"
+                      placeholder={userInfo.dob_month}
+                      // required={true}
+                      value={formData.dob_month}
+                      onChange={handleChange}
+                    />
+                    <input
+                      id="dob_day"
+                      type="number"
+                      name="dob_day"
+                      placeholder={userInfo.dob_day}
+                      // required={true}
+                      value={formData.dob_day}
+                      onChange={handleChange}
+                    />
+                    <input
+                      id="dob_year"
+                      type="number"
+                      name="dob_year"
+                      placeholder={userInfo.dob_year}
+                      // required={true}
+                      value={formData.dob_year}
+                      onChange={handleChange}
+                    />
+                  </div>
 
-            <label>Address</label>
-            <input
-              id="address"
-              type="text"
-              name="address"
-              placeholder={userInfo.address}
-              required={true}
-              value={formData.address}
-              onChange = {handleChange}
-            />
-            <div className='address-multiple-input-container'>
-            <input
-              id="city"
-              type="text"
-              name="city"
-              placeholder={userInfo.city}
-              required={true}
-              value={formData.city}
-              onChange = {handleChange}
-            />
-              <input
-              id="state"
-              type="text"
-              name="state"
-              placeholder={userInfo.state}
-              required={true}
-              value={formData.state}
-              onChange = {handleChange}
-            />
-            <input
-              id="zipcode"
-              type="text"
-              name="zipcode"
-              placeholder={userInfo.zipcode}
-              required={true}
-              value={formData.zipcode}
-              onChange = {handleChange}
-              />
-            </div>
-            <label htmlFor='cell'>Phone Number</label>
-            <input
-              id="cell"
-              type="text"
-              name="cell"
-              placeholder={userInfo.cell}
-              required={true}
-              value={formData.cell}
-              onChange = {handleChange}
-              />
-              </section>
-              <section>
-
-          <label htmlFor='about'>About Me</label>
-            <input
-              id='about'
-              type="text"
-              name={userInfo.about}
-              required={true}
-              value={formData.about}
-              onChange={handleChange}
-            />
-            <label htmlFor='url'>Profile Pirture</label>
-                <input
-                  type="url"
-                  name="url"
-                  id="url"
-                  placeholder={userInfo.url}
-                  value={formData.url}
-              onChange={handleChange}
-              required={true}
-                />
+                  <label>Address</label>
+                  <input
+                    id="address"
+                    type="text"
+                    name="address"
+                    placeholder={userInfo.address}
+                    // required={true}
+                    value={formData.address}
+                    onChange={handleChange}
+                  />
+                  <div className='address-multiple-input-container'>
+                    <input
+                      id="city"
+                      type="text"
+                      name="city"
+                      placeholder={userInfo.city}
+                      // required={true}
+                      value={formData.city}
+                      onChange={handleChange}
+                    />
+                    <input
+                      id="state"
+                      type="text"
+                      name="state"
+                      placeholder={userInfo.state}
+                      // required={true}
+                      value={formData.state}
+                      onChange={handleChange}
+                    />
+                    <input
+                      id="zipcode"
+                      type="text"
+                      name="zipcode"
+                      placeholder={userInfo.zipcode}
+                      // required={true}
+                      value={formData.zipcode}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <label htmlFor='cell'>Phone Number</label>
+                  <input
+                    id="cell"
+                    type="text"
+                    name="cell"
+                    placeholder={userInfo.cell}
+                    // required={true}
+                    value={formData.cell}
+                    onChange={handleChange}
+                  />
                 </section>
-            </form>
+                <section>
+
+                  <label htmlFor='about'>About Me</label>
+                  <input
+                    id='about'
+                    type="text"
+                    name="about"
+                    placeholder={userInfo.about}
+                    // required={true}
+                    value={formData.about}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor='url'>Profile Pirture</label>
+                  <input
+                    type="url"
+                    name="url"
+                    id="url"
+                    placeholder={userInfo.url}
+                    value={formData.url}
+                    onChange={handleChange}
+                    // required={true}
+                    />
+                    <div className='photo-container'>
+              <img src={formData.url} alt="Profile Pic Preview"></img>
+                    </div>
+                
+                  <input type="submit" value = "Update Profile"></input>
+                  </section>
+              </form>
+                  <input type="submit" value = "Delete Account" className='delete'></input>
+            </div>
+          </section>
         </div>
-        </section>
-        </div>
-    {/* <Calendar2 /> */}
-    </div>
-  )
+        {/* <Calendar2 /> */}
+      </div>
+}
+</>)
 }
 
 export default Dashboard
